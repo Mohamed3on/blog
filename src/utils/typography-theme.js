36f925d35914a3331d6bd758f44ff91ce1792efd
Mohamed Oun <mohamed3on@gmail.com>
2020-03-06 12:59:42 +0100

    Add Inter font, use google fonts, update deps

---
+++ src/utils/typography-theme.js
@@ -0,0 +1,89 @@
+import gray from 'gray-percentage';
+
+const MOBILE_MEDIA_QUERY = '@media only screen and (max-width:480px)';
+
+// based on Wordpress Theme 2016
+const theme = {
+  title: 'Wordpress custom theme',
+  baseFontSize: '16px',
+  baseLineHeight: 1.75,
+  scaleRatio: 5 / 2,
+  googleFonts: [
+    {
+      name: 'Inter',
+      styles: ['400', '700'],
+    },
+    {
+      name: 'Merriweather',
+      styles: ['700', '900'],
+    },
+  ],
+  headerFontFamily: ['Merriweather', 'Georgia', 'serif'],
+  bodyFontFamily: ['Inter', 'Georgia', 'serif'],
+  headerWeight: 900,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    blockquote: {
+      ...scale(1 / 5),
+      color: gray(41),
+      fontStyle: 'italic',
+      paddingLeft: rhythm(13 / 16),
+      marginLeft: rhythm(-1),
+      borderLeft: `${rhythm(3 / 16)} solid ${gray(10)}`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: options.bodyColor,
+      fontWeight: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    ul: {
+      listStyle: 'disc',
+    },
+    'ul,ol': {
+      marginLeft: 0,
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      'ul,ol': {
+        marginLeft: rhythm(1),
+      },
+      blockquote: {
+        marginLeft: rhythm(-3 / 4),
+        marginRight: 0,
+        paddingLeft: rhythm(9 / 16),
+      },
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      marginTop: rhythm(2),
+    },
+    h4: {
+      letterSpacing: '0.140625em',
+      textTransform: 'uppercase',
+    },
+    h6: {
+      fontStyle: 'italic',
+    },
+    a: {
+      boxShadow: '0 1px 0 0 currentColor',
+      color: '#007acc',
+      textDecoration: 'none',
+    },
+    'a:hover,a:active': {
+      boxShadow: 'none',
+    },
+    'mark,ins': {
+      background: '#007acc',
+      color: 'white',
+      padding: `${rhythm(1 / 16)} ${rhythm(1 / 8)}`,
+      textDecoration: 'none',
+    },
+  }),
+};
+
+export default theme;

a367ee167a66c502d05e5e8931a8c79929abeeb8
Mohamed Oun <mohamed3on@gmail.com>
2024-09-13 12:33:36 +0200

    add nutridata

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import styled from 'styled-components';
@@ -13,39 +12,77 @@ const Bold = styled.span`
   font-weight: bold;
 `;
 
-const Project = ({ link, title, details }) => (
+const Link = styled.a`
+  color: #0066cc;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const createMarkup = (text, links) => {
+  const parts = text.split(new RegExp(`(${Object.keys(links).join('|')})`, 'g'));
+  return parts.map((part, index) =>
+    links[part] ? (
+      <Link key={index} href={links[part]}>
+        {part}
+      </Link>
+    ) : (
+      part
+    )
+  );
+};
+
+const Project = ({ link, title, details, specialLinks }) => (
   <li>
-    <Bold>{title}: </Bold>
-    {details}.<a href={link}> Check it out.</a>
+    <Link href={link}>
+      <Bold>{title}: </Bold>
+    </Link>
+    {createMarkup(details, specialLinks)}
   </li>
 );
 
-class ProjectsPage extends React.Component {
-  render() {
-    const { data } = this.props;
+const projects = [
+  {
+    title: 'NutriData',
+    link: 'https://getnutridata.com',
+    details: 'A tool to help you understand nutrition data of products on Rewe.de and Amazon.de',
+    specialLinks: {
+      'Rewe.de': 'https://www.rewe.de',
+      'Amazon.de': 'https://www.amazon.de',
+    },
+  },
+  {
+    title: 'Tribe Finder',
+    link: 'https://tribefinder.app',
+    details: 'Discover where your Twitter friends live',
+    specialLinks: {
+      Twitter: 'https://twitter.com',
+    },
+  },
+  {
+    title: 'Topcasts',
+    link: 'https://topcasts.app',
+    details: 'Discover the top podcast episodes shared by people like you',
+    specialLinks: {},
+  },
+];
 
-    const siteTitle = data.site.siteMetadata.title;
+const ProjectsPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title;
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title='Projects' />
-        <h1>Side projects I've built</h1>
-        <List>
-          <Project
-            title='Tribe Finder'
-            link='https://tribefinder.app'
-            details='Discover where your Twitter friends live'
-          ></Project>
-          <Project
-            title='Topcasts'
-            link='https://topcasts.app'
-            details='Discover the top podcast episodes'
-          ></Project>
-        </List>
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title='Projects' />
+      <h1>Side projects I've built</h1>
+      <List>
+        {projects.map((project, index) => (
+          <Project key={index} {...project} />
+        ))}
+      </List>
+    </Layout>
+  );
+};
 
 export default ProjectsPage;
 

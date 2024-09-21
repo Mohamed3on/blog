2ef0b95d9e640c5694640d5d24ba8b9043203263
Mohamed Oun <mohamed3on@gmail.com>
2024-09-21 12:52:22 +0200

    add analytics

--- astro.config.ts
+++ astro.config.ts
@@ -6,14 +6,25 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), sitemap(), mdx()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    sitemap(),
+    mdx(),
+  ],
   markdown: {
     remarkPlugins: [
       remarkToc,

4f6f9715a88a8b2eed1edada2ac48ae433ee9a4f
Mohamed Oun <mohamed3on@gmail.com>
2025-10-02 20:25:07 +0200

    feat: update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -45,6 +45,9 @@ export default defineConfig({
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
+    ssr: {
+      external: ["@resvg/resvg-js"],
+    },
   },
   scopedStyleStrategy: "where",
 });
